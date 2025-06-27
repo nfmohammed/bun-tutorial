29b2992277ed32df4202954fdef9e8b91f0fe5eb
Naseer Feroz Mohammed <naseeruddin.feroz@gmail.com>
2025-06-26 23:59:30 -0500

    lesson-02

---
+++ free-code-camp/lesson-02/README.md
@@ -0,0 +1,15 @@
+# lesson-02
+
+- Running Bun Server
+
+- Adding Figlet dependency 
+
+$ bun add figlet
+$ bun add @types/figlet
+
+- Adding URL Routes and error handling
+
+http://localhost:3000/
+http://localhost:3000/about
+http://localhost:3000/error
+http://localhost:3000/everythingelse
---
+++ free-code-camp/lesson-02/index.ts
@@ -0,0 +1,26 @@
+import figlet from "figlet"
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(request) {
+        const url = new URL(request.url)
+        if (url.pathname === "/") {
+            const body = figlet.textSync("Fun Bun")
+            return new Response(body)
+        }
+        if (url.pathname === "/about"){
+            return new Response("About me!")
+        }
+        if (url.pathname === "/error"){
+            return new Error("Application encountered an error")
+        }
+
+        return new Response("404!")
+    },
+    error(error) {
+        return new Response(`<pre> ${error} \n ${error.stack} </pre>`, {
+            headers: { 'Content-Type': 'text/html'}
+        })
+    }
+})
+console.log(`Server started on http://localhost:${server.port}`)
